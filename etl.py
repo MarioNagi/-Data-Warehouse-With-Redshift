2f2b56983d4ad5b2b2a7d929f775dd347ac77ec9
MarioNagi <Mario_Nagi@yahoo.com>
2022-08-31 02:10:10 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,12 +4,24 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+       This function loads data from AWS S3 to the staging tables in AWS Redshift using COPY command
+    Args:
+        cur: the cursor object.
+         conn = connection to Redshift DB.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+       This function insert data from staging tables to the final tables (fact+dimensions) in AWS Redshift using select command
+    Args:
+        cur: the cursor object.
+         conn = connection to Redshift DB.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
